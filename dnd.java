cb7a06c207ad8ab1d7d7795aa5e6c80d07939d3e
dyl-alex <dalexander_02@arcadia.edu>
2021-12-18 20:50:47 -0500

    pretty much done, few small bugs

--- dnd.java
+++ dnd.java
@@ -1,14 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class dnd {
     public static void main(String args[]) throws FileNotFoundException {
+        Scanner kbReader = new Scanner(System.in);
+
         ArrayList<characters> characterList = new ArrayList<characters>();
 
         setCharacters(characterList);
+        
+        System.out.println("Hello! Welcome to DND. How would you like to sort your characters?");
+        System.out.println("You can sort by Strength, Wisdom, Dexterity, Constitution, Charisma, and Intelligence");
+        String input = kbReader.nextLine();
+        while(true) {
+            sortCharacters(characterList, input.toLowerCase());
 
+            System.out.println("Would you like to sort something else?");
+            System.out.println("Yes or No");
+            input = kbReader.nextLine().toLowerCase();
+            
+            if (input.equals("yes")) {
+                System.out.println("You can sort by Strength, Wisdom, Dexterity, Constitution, Charisma, and Intelligence");
+                input = kbReader.nextLine();
+            } else if (input.equals("no")) {
+                break;
+            } else if (input.equals("recursion!")) {
+                System.out.println("I was stuck on morseCode for almost 2 months");
+            } else {
+                break;
+            }
+        }
     }
 
     public static void setCharacters(ArrayList<characters> characterList) throws FileNotFoundException {
@@ -19,17 +44,134 @@ public class dnd {
             String line = cReader.nextLine();
             if (line.contains("-")) {
                 line.replace(" ", "");
-                characters t = new characters(line.substring(0, line.indexOf("-") - 1), line.substring(line.indexOf("-" + 1), line.length()));
+                characters t = new characters(line.substring(0, line.indexOf("-")), line.substring(line.indexOf("-") + 2, line.length()));
                 for(int i = 0; i < 6; i++) {
                     line = cReader.nextLine();
                     line.replace(" ", "");
                     int x = line.indexOf(":");
-                    String path = line.substring(0, x - 1);
+                    String path = line.substring(0, x);
 
-                }
+                    if (path.toLowerCase().equals("wisdom")) {
+                        t.wisdom = Integer.parseInt(line.substring(x+1));
+                    } else if (path.toLowerCase().equals("strength")) {
+                        t.strength = Integer.parseInt(line.substring(x+1));
+                    } else if (path.toLowerCase().equals("dexterity")) {
+                        t.dexterity = Integer.parseInt(line.substring(x+1));
+                    } else if (path.toLowerCase().equals("charisma")) {
+                        t.charisma = Integer.parseInt(line.substring(x+1));
+                    } else if (path.toLowerCase().equals("constitution")) {
+                        t.constitution = Integer.parseInt(line.substring(x+1));
+                    } else if (path.toLowerCase().equals("intelligence")) {
+                        t.intelligence = Integer.parseInt(line.substring(x+1));
+                    } else {
+                        continue;
+                    }
             }
+            characterList.add(t);
+        } else {
+            continue;
         }
+    }
 
         cReader.close();
     }
+
+    public static void sortCharacters(ArrayList<characters> characterList, String stat) {
+        ArrayList<characters> dupe = characterList;
+        int itt = 0;
+        if (stat.equals("wisdom")) {
+
+            while(!dupe.isEmpty()) {
+                int x = dupe.get(0).wisdom;
+                itt = 0;
+                for(int i = 0; i < dupe.size(); i++) {
+                    if (x < dupe.get(i).wisdom) {
+                        x = dupe.get(i).wisdom;
+                        itt = i;
+                    }
+                }
+                System.out.println(dupe.get(itt).name + ": " + dupe.get(itt).wisdom);
+                dupe.remove(itt);
+            }
+
+        } else if (stat.equals("strength")) {
+
+            while(!dupe.isEmpty()) {
+                int x = dupe.get(0).strength;
+                itt = 0;
+                for(int i = 0; i < dupe.size(); i++) {
+                    if (x < dupe.get(i).strength) {
+                        x = dupe.get(i).strength;
+                        itt = i;
+                    }
+                }
+                System.out.println(dupe.get(itt).name + ": " + dupe.get(itt).strength);
+                dupe.remove(itt);
+            }
+
+        } else if (stat.equals("dexterity")) {
+
+            while(!dupe.isEmpty()) {
+                int x = dupe.get(0).dexterity;
+                itt = 0;
+                for(int i = 0; i < dupe.size(); i++) {
+                    if (x < dupe.get(i).dexterity) {
+                        x = dupe.get(i).dexterity;
+                        itt = i;
+                    }
+                }
+                System.out.println(dupe.get(itt).name + ": " + dupe.get(itt).dexterity);
+                dupe.remove(itt);
+            }
+
+        } else if (stat.equals("constitution")) {
+
+            while(!dupe.isEmpty()) {
+                int x = dupe.get(0).constitution;
+                itt = 0;
+                for(int i = 0; i < dupe.size(); i++) {
+                    if (x < dupe.get(i).constitution) {
+                        x = dupe.get(i).constitution;
+                        itt = i;
+                    }
+                }
+                System.out.println(dupe.get(itt).name + ": " + dupe.get(itt).constitution);
+                dupe.remove(itt);
+            }
+
+        } else if (stat.equals("charisma")) {
+
+            while(!dupe.isEmpty()) {
+                int x = dupe.get(0).charisma;
+                itt = 0;
+                for(int i = 0; i < dupe.size(); i++) {
+                    if (x < dupe.get(i).charisma) {
+                        x = dupe.get(i).charisma;
+                        itt = i;
+                    }
+                }
+                System.out.println(dupe.get(itt).name + ": " + dupe.get(itt).charisma);
+                dupe.remove(itt);
+            }
+
+        } else if (stat.equals("intelligence")) {
+
+            while(!dupe.isEmpty()) {
+                int x = dupe.get(0).intelligence;
+                itt = 0;
+                for(int i = 0; i < dupe.size(); i++) {
+                    if (x < dupe.get(i).intelligence) {
+                        x = dupe.get(i).intelligence;
+                        itt = i;
+                    }
+                }
+                System.out.println(dupe.get(itt).name + ": " + dupe.get(itt).intelligence);
+                dupe.remove(itt);
+            }
+
+        } else {
+            System.out.println("not sure what you mean by that");
+        }
+    }
+    
 }
